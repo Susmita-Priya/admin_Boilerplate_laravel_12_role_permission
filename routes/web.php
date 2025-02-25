c14f391a1e669f78271ab1ce983cb8c90166b272
Susmita-Priya <susmita.saha.cs@gmail.com>
2025-02-25 15:20:45 +0600

    Boilerplate done

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
+Route::middleware('auth')->group(callback: function () {
 
     Route::get('/index', [IndexController::class, 'adminIndex'])->name("index");
 
